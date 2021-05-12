f5faad2ba113979625d1379fea99d1f7c475450d
Nikita Mittal <nikitamittal2423@gmail.com>
2021-05-12 19:11:40 +0530

    Publishing library

--- src/index.js
+++ src/index.js
@@ -1,6 +1,1 @@
-import emojiIndex from './utils/emoji-index'
-import store from './utils/store'
-import frequently from './utils/frequently'
-
-export { Picker, Emoji, Category, Parser } from './components'
-export { emojiIndex, store, frequently }
+export { Picker, Emoji, Category, Parser } from "./components";
